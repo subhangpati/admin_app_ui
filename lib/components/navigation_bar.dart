aaeb5a15a27d1385550354c1d7659c8c5432beee
Subhang <https://github.com/subhangpati>
2020-09-27 16:47:26 +0530

    bottomNav , sidebar , setting to be done

--- lib/components/navigation_bar.dart
+++ lib/components/navigation_bar.dart
@@ -6,15 +6,21 @@ import '../constants.dart';
 AppBar buildAppBar() {
     return AppBar(
       backgroundColor: kBarColor,
-      leading: IconButton(icon: Icon(Icons.menu), onPressed: () {  } ,
-      ),
       title: Text("Admin Dashboard",
-      style: TextStyle(
-      fontFamily: 'Inter', fontWeight: FontWeight.bold ),),
+        style: TextStyle(
+            fontFamily: 'Inter', fontWeight: FontWeight.bold ),),
       actions: <Widget>[
         IconButton(icon: Icon(Icons.settings),
-        onPressed: (){},)
+          onPressed: (){},)
       ],
     );
   }
-
+//
+// title: Text("Admin Dashboard",
+// style: TextStyle(
+// fontFamily: 'Inter', fontWeight: FontWeight.bold ),),
+// actions: <Widget>[
+// IconButton(icon: Icon(Icons.settings),
+// onPressed: (){},)
+// ],
+// );
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:admin_app_ui/components/body.dart';
 import 'package:admin_app_ui/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 class Homepage extends StatelessWidget {
@@ -12,6 +13,25 @@ class Homepage extends StatelessWidget {
         return Scaffold(
           appBar: buildAppBar(),
           body: Body(),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: kBarColor,
+                  ),
+                ),
+                sideBarMenuItems(label: 'Bookings', imageData: 'event_note-24px.svg', press: (){},),
+                sideBarMenuItems(label: 'Booking Calander', imageData: 'date_range-24px.svg', press: (){},),
+                sideBarMenuItems(label: 'Add/Remove Package', imageData: 'box.svg', press: (){},),
+                sideBarMenuItems(label: 'Add/Remove Services', imageData: 'service.svg', press: (){},),
+                sideBarMenuItems(label: 'Add/Remove Sliders', imageData: 'slider-tool.svg', press: (){},),
+                sideBarMenuItems(label: 'Time Management', imageData: 'clock.svg', press: (){},),
+                sideBarMenuItems(label: 'Logout', imageData: 'logout.svg', press: (){},),
+              ],
+            ),
+          ),
           bottomNavigationBar: Container(
             padding: EdgeInsets.symmetric(vertical: 10 , horizontal: 20),
             height: 60.0,
@@ -31,6 +51,33 @@ class Homepage extends StatelessWidget {
 
 }
 
+
+// below this is the code for the sidebar content or ( list )
+class sideBarMenuItems extends StatelessWidget {
+  sideBarMenuItems( {this.label , this.imageData , this.press});
+  final String label ;
+  final Function press ;
+  final String imageData ;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: SvgPicture.asset('images/$imageData' , width: 25.0, height: 25.0 ),
+        title: Text( label , style: TextStyle(
+          fontSize: 12.0,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Inter'
+
+        ),
+        ),
+        onTap: press,
+      ),
+    );
+  }
+}
+
+// The following is the abstracted code for the bottom nav icons:
+
 class bottom_nav_icons extends StatelessWidget {
   bottom_nav_icons({this.icons , this.on_Press});
   final Icon icons ;
@@ -38,7 +85,7 @@ class bottom_nav_icons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: on_Press,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
@@ -47,3 +94,5 @@ class bottom_nav_icons extends StatelessWidget {
     );
   }
 }
+
+
