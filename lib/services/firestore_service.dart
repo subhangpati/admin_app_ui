cdf8e74cd99f8e835e8bc7f11f5c9a2ed918f09a
subhangpati <subhangpati4@gmail.com>
2020-11-24 23:45:41 +0530

    CRUD and dynamic path changing onGoing process.

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,27 +1,39 @@
 import 'package:admin_app_ui/model/product_model.dart';
+import 'package:admin_app_ui/package/ClassicPackage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 
-class FirestoreService {
-  FirebaseFirestore _db = FirebaseFirestore.instance;
-  String collection;
-  String PackageName;
-  String CollectionName = "services/collection/PackageName";
+class FirestoreService with ChangeNotifier{
+  String subPackage;
+  FirestoreService({this.subPackage});
 
 
+  //Stream of services
+  Stream<QuerySnapshot> getStreamServices() {
+    return FirebaseFirestore.instance.collection('services').snapshots();
+  }
+
+  // Stream of Packages
+  Stream<QuerySnapshot> getStreamPackage(String package) {
+    return FirebaseFirestore.instance
+        .collection('/services/$package/$subPackage')
+        .snapshots();
+  }
+}
   //GET PACKAGES
   Stream<List<Product>> getProduct(){
-    return FirebaseFirestore.instance.collection(CollectionName).snapshots().map((snapshot) => snapshot.docs.map((document) => Product.fromFirestore(document.data())).toList());
+    return FirebaseFirestore.instance.collection('/services/$package/$subPackage').snapshots().map((snapshot) => snapshot.docs.map((document) => Product.fromFirestore(document.data())).toList());
   }
 
   //DELETE PACKAGE
   Future<void> removeProduct(String title){
-    return _db.collection(CollectionName).doc(title).delete();
+    return  FirebaseFirestore.instance.collection('/services/$package/$subPackage').doc(title).delete();
   }
 
 
   // ADD PACKAGE
   Future<void> saveProduct(Product product){
-    return _db.collection(CollectionName).doc(product.title).set(product.toMap());
+    return  FirebaseFirestore.instance.collection('/services/$package/$subPackage').doc(product.title).set(product.toMap());
   }
 
 }
