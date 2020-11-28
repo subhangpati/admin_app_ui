c9ad539d60efda6bb1984861bfb0949cc5b9c68b
subhangpati <subhangpati4@gmail.com>
2020-11-28 21:41:35 +0530

    addService.dart line 169 .

--- lib/screens/addService.dart
+++ lib/screens/addService.dart
@@ -1,5 +1,7 @@
 import 'package:admin_app_ui/constants.dart';
+import 'package:admin_app_ui/model/product_model.dart';
 import 'package:admin_app_ui/productProvider/EditItems.dart';
+import 'package:admin_app_ui/services/firestore_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -23,6 +25,8 @@ class _addServicePageState extends State<addServicePage> {
     _titleController.text = '';
     super.initState();
   }
+
+  final firestoreService = FirestoreService();
 
   @override
   Widget build(BuildContext context) {
@@ -48,121 +52,133 @@ class _addServicePageState extends State<addServicePage> {
       ),
       body: Container(
         padding: EdgeInsets.all(18),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Title',
-                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 3 / 5,
-                    child: TextFormField(
-                      controller: _titleController,
-                      onChanged: (value) {
-                        value1 = value;
-                        print('$value');
-                      },
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Title',
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 3 / 5,
+                      child: TextFormField(
+                        controller: _titleController,
+                        onSaved: (value){
+                          ProductProvider().changeTitle(value: _titleController.text);
+                          print(_titleController.text);
+                        },
+                        // onChanged: (value) {
+                        //   value1 = value;
+                        //   print('$value');
+                        // },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Details',
-                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 3 / 5,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Details',
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 3 / 5,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Current Price',
-                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 3 / 5,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Current Price',
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 3 / 5,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Previous Price',
-                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 3 / 5,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Previous Price',
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 3 / 5,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Time',
-                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 3 / 5,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Time',
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 3 / 5,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            RaisedButton(
-              onPressed: () {
-                ProductProvider().changeTitle(value: value1);
-                ProductProvider().saveProduct();
-              },
-              child: Text('Send Data'),
-            )
-          ],
+              RaisedButton(
+                onPressed: () {
+                  ProductProvider().saveProduct();
+                  var newProduct = Product(
+                      title: _titleController.text,
+                      time: '',
+                      details: 'details',
+                      currentPrice: '',
+                      previousPrice: '');
+                  firestoreService.saveProduct(product: newProduct);
+                },
+                child: Text('Send Data'),
+              )
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomHomeNavButton(),
